578a04f42082d0571d6c365ba03cf30c8cffb10e
Andika Dwiki Darmawan <andika0106dwiki@gmail.com>
2024-04-25 16:31:14 +0700

    database struktur

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
 
-Route::get('/timeline', TimelineController::class)->name('timeline');
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/timeline', TimelineController::class)->name('timeline');
+});
+
+Route::middleware('guest')->group(function () {
+});
 
 require __DIR__ . '/auth.php';
