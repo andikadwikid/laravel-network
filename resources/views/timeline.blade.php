d9ee0b670644bff16cebdae4c7b0713c82854fba
Andika Dwiki Darmawan <andika0106dwiki@gmail.com>
2024-04-26 16:55:05 +0700

    design form untuk post status

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -2,35 +2,60 @@
     <x-container>
         <div class="grid grid-cols-12 gap-6">
             <div class="col-span-8">
-                <div class="space-y-6">
-                    <div class="border rounded-xl p-5 space-y-5">
-                        @foreach ($statuses as $status)
-                            <div class="flex">
-                                <div class="flex-shrink-0 mr-3">
-                                    <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150"
-                                        alt="{{ $status->user->name }}">
+                <x-card>
+                    <form action="" method="post">
+                        <div class="flex">
+                            <div class="flex-shrink-0 mr-3">
+                                <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="">
+                            </div>
+                            <div class="w-full">
+                                <div class="font-semibold">
+                                    {{ Auth::user()->name }}
                                 </div>
-                                <div>
-                                    <div class="font-semibold">
-                                        {{ $status->user->name }}
-                                    </div>
-                                    <div class="leading-relaxed">
-                                        {{ $status->body }}
-                                    </div>
-                                    <div class="text-sm text-gray-600">
-                                        {{ $status->created_at->diffForHumans() }}
-                                    </div>
+                                <div class="my-2">
+                                    <textarea name="body"
+                                        class="form-textarea w-full border-gray-300 rounded-xl resize-none focus:border-blue-500 focus:ring focus:ring-blue-200 transition duration-200"
+                                        id="body" placeholder="What is on your mind ?"></textarea>
+                                </div>
+                                <div class="text-right">
+                                    <x-button>
+                                        Post
+                                    </x-button>
                                 </div>
                             </div>
+                        </div>
+                    </form>
+                </x-card>
+                <div class="space-y-6 mt-5">
+                    <div class="space-y-5">
+                        @foreach ($statuses as $status)
+                            <x-card>
+                                <div class="flex">
+                                    <div class="flex-shrink-0 mr-3">
+                                        <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150"
+                                            alt="{{ Auth::user()->name }}">
+                                    </div>
+                                    <div>
+                                        <div class="font-semibold">
+                                            {{ $status->user->name }}
+                                        </div>
+                                        <div class="leading-relaxed">
+                                            {{ $status->body }}
+                                        </div>
+                                        <div class="text-sm text-gray-600">
+                                            {{ $status->created_at->diffForHumans() }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </x-card>
                         @endforeach
                     </div>
                 </div>
             </div>
             <div class="col-span-4">
-                <div class="border p-5 rounded-xl">
+                <x-card>
                     <h1 class="font-semibold mb-5">Recently follows</h1>
                     <div class="space-y-6">
-                        {{-- {{ Auth::user()->follows }} --}}
                         @foreach (Auth::user()->follows()->limit(5)->get() as $user)
                             <div class="flex items-center">
                                 <div class="flex-shrink-0 mr-3">
@@ -48,7 +73,7 @@
                             </div>
                         @endforeach
                     </div>
-                </div>
+                </x-card>
             </div>
         </div>
     </x-container>
